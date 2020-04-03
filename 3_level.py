a37730e9578cdc383bc4673c455879f4b4ab92f3
Vinsent <pasha.do@yandex.ua>
2020-04-03 21:22:10 +0300

    add 3 level and file for it

---
+++ 3_level.py
@@ -0,0 +1,11 @@
+fileWithNumbers = open("file_for_3_level.txt", "r")
+
+symbolForEnd = input("Ведіть символ: ")
+
+for x in fileWithNumbers:
+   # print(x)
+    if x.endswith(symbolForEnd,0,-1):
+        print(x[0:-1].swapcase())
+        continue
+    elif x.endswith(symbolForEnd):
+        print(x.swapcase())
